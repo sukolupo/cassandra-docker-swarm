e3d822de8ddc4bade67099cb178daff7cf11a5e9
sukolupo <git@sukolupo.com>
2019-02-10 22:19:25 +0000

    tweaks

--- roles/build/files/custom-entrypoint.sh
+++ roles/build/files/custom-entrypoint.sh
@@ -9,18 +9,20 @@
 set -e
 
 ## lets look for other tasks
-cassandra_tasks=$(getent hosts tasks.cassandra | awk '{print $1}' ORS=', ' | sed s'/..$//')
-cassandra_ip=$(hostname --all-ip-address | awk '{print $1}' ORS=', ' | sed s'/..$//')
+
+cassandra_ip=$(hostname --ip-address)
+cassandra_tasks=$(getent hosts tasks.cassandra | awk '{print $1}' ORS=',\n' | sed "/$cassandra_ip/d" )
 
 # TODO: add automatic rack placement based on node counts
+#export CASSANDRA_SEEDS="auto"
 
-if [ -z "$cassandra_tasks" ]
+if [ ! -z "$cassandra_tasks" ]
 then
-    echo "First node in the cluster"
-else
-    CASSANDRA_SEEDS=$cassandra_tasks
+    export CASSANDRA_SEEDS=$(echo $cassandra_tasks | sed s'/,$//')
 fi
 
+#export CASSANDRA_LISTEN_ADDRESS=$cassandra_ip
+#export CASSANDRA_BROADCAST_ADDRESS=$cassandra_ip
 
 # call the official docker entrypoint
 /docker-entrypoint.sh cassandra -f
